44edc56c046d09e49735f61e2f25f15c54f1faab
Dmitry Dmitruk <fartovyitigr@gmail.com>
2023-01-29 19:44:23 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит
+число A в натуральную степень B.
+Нельзя использовать библиотеку Math!
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+Console.Clear();
+
+
+int naturalsqrt(int a,int b)
+{
+    int count =1;
+    int y =1;
+      while (count<b+1)
+      {
+        y=y*a;
+        count++;
+      }
+    return y;
+}
+Console.WriteLine("Введите число a");
+int a=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b");
+int b= int.Parse(Console.ReadLine()!);
+int x=naturalsqrt( a,b);
+
+Console.WriteLine($"ответ {x}");
