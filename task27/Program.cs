2568650003490a56323af53d9f5d543bee7bbca9
Dmitry Dmitruk <fartovyitigr@gmail.com>
2023-01-30 01:49:43 +0300

    task23

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Clear();
+int numbersum (int number)
+{
+    int sum=0;
+    
+    
+    for (int i = number/10; i >0; i++)
+    {
+       sum+=number%10; 
+    }
+       
+     return sum;
+}
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine()!);
+
+int b =numbersum(a);
+Console.WriteLine(b);
---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив 
+из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.Clear();
+int[] ReadArray()
+{
+  Console.WriteLine("Введите длинну массива");
+  int n = int.Parse(Console.ReadLine()!);
+  int[] massiv = new int [n];
+  
+    for (int i = 0; i < n; i++)
+      {
+    massiv[i]=int.Parse(Console.ReadLine()!);
+      }
+      return massiv;
+
+}
+int[]array=ReadArray();
+void PrintArray(int[] col)
+{
+    int position =col.Length;
+    int count = 0;
+    while (count<position)
+    {
+      Console.WriteLine(col[position]);
+      count++;
+    }
+}
+PrintArray(array);
